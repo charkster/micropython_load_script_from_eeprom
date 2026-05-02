3a0d702806614d8909e4565e04b895999167148b
charkster <31560568+charkster@users.noreply.github.com>
2026-05-02 15:36:31 -0700

    Update i2c_read_and_run_script_from_eeprom.py

--- i2c_read_and_run_script_from_eeprom.py
+++ i2c_read_and_run_script_from_eeprom.py
@@ -1,9 +1,9 @@
 import machine
 import time
 
-time.sleep(2)
+time.sleep(2) # time to halt execution, for debugging
 
-i2c = machine.I2C(sda=machine.Pin(6), scl=machine.Pin(7), freq=400_000)
+i2c = machine.I2C(sda=machine.Pin(6), scl=machine.Pin(7), freq=400_000) # Seeed Xiao ESP32C3
 END_MARKER = "#END\n"
 
 def read_script_from_i2c(i2c=i2c, addr=0x50):

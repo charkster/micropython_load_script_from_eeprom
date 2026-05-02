aec6e703e885a2e5057984e22aef2c54fc82f90d
charkster <31560568+charkster@users.noreply.github.com>
2026-05-02 15:28:07 -0700

    Update i2c_write_script_to_eeprom_from_file.py

--- i2c_write_script_to_eeprom_from_file.py
+++ i2c_write_script_to_eeprom_from_file.py
@@ -1,7 +1,7 @@
 import machine
 import time
 
-class CAT24C512:
+class EEPROM: # generic 
 
     # Constructor
     def __init__(self, i2c):
@@ -10,13 +10,14 @@ class CAT24C512:
         self.addrsize  = 16
         self.page_size = 128
         self.num_pages = 512
+        self.page_write_delay = 0.01 # in seconds
        
     def write_data(self, address=0x0000, data=''):
         num_data_pages  = len(data) // self.page_size
         num_data_remain = len(data) % self.page_size 
         for page in range(0,num_data_pages):
             self.i2c.writeto_mem(self.slave_id, page*self.page_size, bytearray(data[page*self.page_size : (page+1)*self.page_size], "ascii"), addrsize=self.addrsize)
-            time.sleep(0.01)
+            time.sleep(self.page_write_delay)
         self.i2c.writeto_mem(self.slave_id, num_data_pages*self.page_size, bytearray(data[num_data_pages*self.page_size : num_data_pages*self.page_size + num_data_remain], "ascii"), addrsize=self.addrsize)
         
     def read_data(self, address=0x0000, num_bytes=1):
@@ -27,5 +28,5 @@ f = open('script.py', 'r') # the file script.py is saved to the MCU flash memory
 script = f.read() + "#END\n"
 
 i2c = machine.I2C(1, scl=machine.Pin(7), sda=machine.Pin(6), freq=400_000)
-eeprom = CAT24C512(i2c=i2c)
+eeprom = EEPROM(i2c=i2c)
 eeprom.write_data(data=script)
